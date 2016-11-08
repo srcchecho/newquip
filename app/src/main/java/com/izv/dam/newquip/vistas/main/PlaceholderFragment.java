f09ed302afbc4bc76943f43f12cc51cd2a37bf58
sergio <sergio23rc@gmail.com>
2016-11-08 14:43:46 +0100

    dia 8 2

---
+++ app/src/main/java/com/izv/dam/newquip/vistas/main/PlaceholderFragment.java
@@ -0,0 +1,34 @@
+package com.izv.dam.newquip.vistas.main;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.izv.dam.newquip.R;
+
+/**
+ * Created by dam on 08/11/2016.
+ */
+
+public class PlaceholderFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceholderFragment() {
+    }
+
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
+        return rootView;
+    }
+}
